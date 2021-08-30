3ca7237ce89e3a9d62f21c4a5839cabe5c421c03
Polina Skrobot <chi.polly.no@gmail.com>
2021-08-30 21:45:05 +0000

    urls_index added to organise urls

--- express_server.js
+++ express_server.js
@@ -9,6 +9,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -17,6 +18,10 @@ app.get("/urls.json", (req, res) => {
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
