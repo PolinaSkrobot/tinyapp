7745670533164eba505748fad1cd7613b2cfeacb
Polina Skrobot <chi.polly.no@gmail.com>
2021-09-01 23:57:49 +0000

    Registration/login

--- express_server.js
+++ express_server.js
@@ -15,6 +15,21 @@ let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+let users = {
+  '14512': {
+    id: "14512",
+    email: "a@gmail.com",
+    password: "helloHELLO"
+  },
+  '14513': {
+    id: "14513",
+    email: "aba@gmail.com",
+    password: "byeBYE"
+  },
+
+};
+
 function generateRandomString() { //random short URL
   let string = '';
   const characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -25,14 +40,48 @@ function generateRandomString() { //random short URL
   }
   return string;
 }
+function foundUser(userID) {
+  for (const key in users) {
+    const user = users[key];
+    if (user.id === userID) {
+      return user;
+    }
+  }
+  return "no there";
+   
+}
+
+function getTheUserFromEmail(email)  {
+  for (const key in users) {
+    const user_ = users[key];
+    if (user_.email === email) {
+      return user_;
+    }
+  }
+  return false;
+}
 
 app.post('/login',(req,res) => { // Login functionality
-  res.cookie("username", req.body.username);
+  const email = req.body.email;
+  const password = req.body.password;
+  if (!email || !password) {
+    return res.status(400).send('Empty inputs');
+  }
+  const user = getTheUserFromEmail(email);
+  
+  if (!user) {
+    return res.status(403).send('no user with that email found');
+  }
+  if (user.password !== password) {
+    return res.status(403).send('password does not match');
+  }
+
+  res.cookie("user_id", user.id);
   res.redirect('/urls');
 });
 
 app.post('/logout',(req,res) => {// Logout
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
 });
 
@@ -60,6 +109,37 @@ app.post('/urls/:shortURL/edit',(req,res) => {//press EDIT button on the url pag
   res.redirect(shortURL);
 });
 
+app.get('/register', (req,res) => {
+  const templateVars = { urls: urlDatabase , user: foundUser(req.cookies["user_id"])};
+  res.render('registration', templateVars);
+});
+app.get('/login', (req,res)=> {
+  const templateVars = { urls: urlDatabase , user: foundUser(req.cookies["user_id"])};
+  res.render('login', templateVars);
+});
+
+app.post('/register', (req,res) => { //Registration functionality
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  if (!email || !password) {
+    return res.status(400).send('Empty inputs');
+  } else if (getTheUserFromEmail(email)) {
+    return res.status(400).send('This email has already been used');
+  }
+
+  let newID = generateRandomString();
+  users[newID] = {
+    id: newID,
+    email: req.body.email,
+    password: req.body.password
+  };
+  
+  res.cookie("user_id", newID);
+  res.redirect('/urls');
+  
+});
+
 app.get("/", (req, res) => { //home page
   res.send("Hello!");
 });
@@ -69,19 +149,21 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => { //urls page
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  console.log("__________", req.cookies["user_id"]);
+  console.log('******', foundUser(req.cookies["user_id"]));
+  const templateVars = { urls: urlDatabase, user: foundUser(req.cookies["user_id"])  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => { // new URL
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { user: foundUser(req.cookies["user_id"])  };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
-  const templateVars = { shortURL, longURL, username: req.cookies["username"]};
+  const templateVars = { shortURL, longURL, user: foundUser(req.cookies["user_id"])  };
   
   if (!urlDatabase[shortURL]) { //edge case with not existing shortURL
     res.redirect('/urls');
@@ -96,9 +178,7 @@ app.get("/u/:shortURL", (req, res) => { //redirection from short URL to original
   res.redirect(longURL);
 });
 
-app.get('/register', (req,res)=>{
-  res.render('registration');
-});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
